2a95a440de6d274d466ac85d43ae6f79739908bb
Eugen <eugen.bilenko@gmail.com>
2021-07-29 09:53:09 +0300

    added bearer auth to swagger

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .setTitle('Auth')
     .setDescription('Demo auth')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
